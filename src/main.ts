5a5d71fdeae14a05d96b5ff061555ca730cf086d
Modupe Ajibola <deeajibola@gmail.com>
2021-04-23 17:43:56 -0500

    update app v3

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const { PORT = 5000 } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix("api/v1");
   app.setGlobalPrefix("");
   await app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
