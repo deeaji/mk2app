ff2d33d52c4f26d840fdab43c170ad97430e4673
Modupe Ajibola <deeajibola@gmail.com>
2021-04-23 17:43:24 -0500

    update app v2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require("dotenv").config();
 
 const { PORT = 5000 } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/v1");
+  app.setGlobalPrefix("");
   await app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
